12cb70197743926b5038b37295e6469cdfd47182
itaybroder <itay.broder@gmail.com>
2022-01-06 05:54:06 +0200

    ddfd

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+HOME_DIRECTORY = '/home/itaybroder/Desktop/netchop_pipeline'
+INPUT_DIR = os.path.join(HOME_DIRECTORY, 'input_files')
+OUTPUT_DIR = os.path.join(HOME_DIRECTORY, 'output_files')
+PATH_TO_NETCHOP = '/home/itaybroder/Documents/netchop/netchop-3.1'
--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import os
 
 from netchop import create_dataframe, feed_to_Netchop
 from MHCpan import split_for_all_peptides
+from config import HOME_DIRECTORY, INPUT_DIR, OUTPUT_DIR, PATH_TO_NETCHOP
 
-HOME_DIRECTORY = '/home/itaybroder/Desktop/netchop_pipeline'
-INPUT_DIR = os.path.join(HOME_DIRECTORY, 'input_files')
-OUTPUT_DIR = os.path.join(HOME_DIRECTORY, 'output_files')
-PATH_TO_NETCHOP = '/home/itaybroder/Documents/netchop/netchop-3.1'
 
 #choose you can choose the RNA either from here or through the .fasta file
 mutent_dict = {
@@ -31,7 +28,10 @@ input_file.close()
 
 
 def netchop_mutation_pipeline(mutation_dict):  
+    #creating all peptides from RNA
     a1 = split_for_all_peptides(mutent_dict['seq1'])
+
+    #creating a dataframe with all the peptides and positions and adding a "choped" column
     cols1 = ['peptide', 'start_pos','end_pos']
     df1 =  pd.DataFrame(a1, columns=cols1)
     df1["Chopped"] = ''
@@ -41,12 +41,13 @@ def netchop_mutation_pipeline(mutation_dict):
     df2['Chopped'] = ''
 
     #add choped indicator to the df
-    input_folder = INPUT_DIR +"/spike.fasta"
-    output_folder = OUTPUT_DIR + "/out.txt"
-    feed_to_Netchop(input_folder, output_folder, PATH_TO_NETCHOP)
-    df1 = create_dataframe(output_folder, df1, 'seq1', HOME_DIRECTORY)
-    df2 = create_dataframe(output_folder, df2, 'seq2', HOME_DIRECTORY)
-        
+    input_file = INPUT_DIR +"/spike.fasta"
+    output_file = OUTPUT_DIR + "/out.txt"
+    feed_to_Netchop(input_file, output_file, PATH_TO_NETCHOP)
+    df1 = create_dataframe(output_file, df1, 'seq1', OUTPUT_DIR)
+    df2 = create_dataframe(output_file, df2, 'seq2', OUTPUT_DIR)
+    
+    
     result = df1.merge(df2, how='left', left_on=['start_pos', 'end_pos'], right_on=['start_pos','end_pos'], suffixes=['_before_mutation', '_after_mutation'])
     return result
     
