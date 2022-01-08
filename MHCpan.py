13fde8956035440949b15e2bdd616e467d759844
itaybroder <itay.broder@gmail.com>
2022-01-08 15:06:33 +0200

    jhgjgh

--- MHCpan.py
+++ MHCpan.py
@@ -17,8 +17,6 @@ def split_for_all_peptides(str):
     return split_by_k(str, 8) + split_by_k(str, 9) + split_by_k(str, 10)
 
 
-
-
 def feed_to_NetMHCPan(input_file_path, output_file_path, pep_length):
     path_to_tool = '/home/itaybroder/Desktop/netMHCpan-4.1'
     if os.path.exists(path_to_tool) and os.path.exists(output_file_path) and os.path.exists(input_file_path):
@@ -36,4 +34,4 @@ def feed_to_NetMHCPan(input_file_path, output_file_path, pep_length):
 input_folder = "/home/itaybroder/Desktop/CovidResearch/CovSpikeMCHProject/input_files"
 output_folder = "/home/itaybroder/Desktop/CovidResearch/CovSpikeMCHProject/output_files"
 pep_length = [8, 9, 10]
-#feed_to_NetMHCPan(input_folder + '/all_peps.txt', output_folder + '/out.txt', pep_length)
+feed_to_NetMHCPan(input_folder + '/all_peps.txt', output_folder + '/out.txt', pep_length)
